e3b0dbad307b37ac0bd0df51ce3101ee8e0955d5
thabisani <thabisonsibz@gmail.com>
2022-07-06 18:02:51 +0400

    Add ML

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
 from flask import Flask, request
 
+import pandas as pd
+from scipy.sparse import csr_matrix
+from sklearn.neighbors import NearestNeighbors
+from fuzzywuzzy import process
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return {"about":"Hello Flask App!!!"}
+    restaurants='restaurants.csv'
+
+    df_restaurants=pd.read_csv(restaurants, usecols=['id','bna'], dtype={'id':'int32','bna':'str'})
+    df_recommendations=pd.read_csv(restaurants, usecols=['bid','id','Lon', 'Lat'],dtype={'bid':'int32','id':'int32','Lon':'float32', 'Lat':'float32'})
+
+    restaurants_users=df_recommendations.pivot(index='id', columns='bid',values=['Lon', 'Lat']).fillna(0)
+    mat_restaurants_users=csr_matrix(restaurants_users.values)
+
+    model_knn= NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=20)
+
+    model_knn.fit(mat_restaurants_users)
+
+    def recommender(restaurant_name, data,model, n_recommendations ):
+        model.fit(data)
+        idx=process.extractOne(restaurant_name, df_restaurants['bna'])[2]
+        print('Restaurant Selected: ',df_restaurants['bna'][idx], 'Index: ',idx)
+        print('Searching for recommendations.....')
+        distances, indices=model.kneighbors(data[idx], n_neighbors=n_recommendations)
+        recommendations = []
+        for i in indices:
+            bnas = df_restaurants['bna'][i].where(i!=idx)
+            ids = df_restaurants['id'][i].where(i!=idx)
+            recommendations.append({"id": str(ids)})
+            print(bnas)
+            print(ids)
+        return recommendations
+    recommendations = recommender('KFC', mat_restaurants_users, model_knn,20)
+    return {"recommended_restaurants": recommendations}
 
 @app.route('/healthy', methods=['GET', 'POST'])
 def healthy():
